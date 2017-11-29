d2ae0b8b966c320cb8e46f6857ac50242e5712ee
Iain McCown <iain.mccown@ibm.com>
2017-11-29 17:57:50 -0600

    first commit

---
+++ Core ML Vision Continuous Learning/Core ML Vision Continuous Learning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core ML Vision Continuous Learning
+//
+//  Created by Iain McCown on 11/29/17.
+//  Copyright © 2017 Iain McCown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
