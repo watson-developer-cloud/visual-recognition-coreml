6f558cca2e69a932e02c4034c249399e96d63ada
Iain McCown <iain.mccown@ibm.com>
2017-11-30 13:22:23 -0600

    Remove references to other models

--- Core Ml Vision Simple/Core Ml Vision Simple/ImageClassificationViewController.swift
+++ Core Ml Vision Simple/Core Ml Vision Simple/ImageClassificationViewController.swift
@@ -23,14 +23,7 @@ class ImageClassificationViewController: UIViewController {
     /// - Tag: MLModelSetup
     lazy var classificationRequest: VNCoreMLRequest = {
         do {
-            /*
-             Use the Swift class `MobileNet` Core ML generates from the model.
-             To use a different Core ML classifier model, add it to the project
-             and replace `MobileNet` with that model's generated Swift class.
-             */
             let model = try VNCoreMLModel(for: MobileNet().model)
-            //let model = try VNCoreMLModel(for: watsonVR().model)
-            //let model = try VNCoreMLModel(for: custom_pipeline_classifier().model)
             
             let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
                 self?.processClassifications(for: request, error: error)
