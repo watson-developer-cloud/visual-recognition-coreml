104c0f7223aff28303c9a59ccb40e7ce44f5ff3a
Iain McCown <iain.mccown@ibm.com>
2017-12-11 18:03:32 -0600

    Change project names

--- Core ML Vision Custom/Core ML Vision Custom/ImageClassificationViewController.swift
+++ Core ML Vision Custom/Core ML Vision Custom/ImageClassificationViewController.swift
@@ -19,19 +19,15 @@ class ImageClassificationViewController: UIViewController {
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var classificationLabel: UILabel!
     
-    let apiKey = ""
-    let classifierId = ""
-    let version = ""
+    let apiKey = "9a88901d-b8ff-4e5b-bf64-e0ad07e8eb0d"
+    let classifierId = "connectors"
+    let version = "2017-12-07"
     var visualRecognition: VisualRecognition!
-//    var watsonModel: VisualRecognitionCoreMLModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.visualRecognition = VisualRecognition(apiKey: apiKey, version: version)
-        
-        if let model = try? VNCoreMLModel( for: MobileNet().model ) {
-//            watsonModel = VisualRecognitionCoreMLModel( model: model )
-        }
+        visualRecognition.updateCoreMLModelLocally(classifierID: "connectors", apiKey: apiKey)
     }
     
     
@@ -76,21 +72,44 @@ class ImageClassificationViewController: UIViewController {
             print(error)
         }
         
+        let fileManager = FileManager.default
+        let applicationSupportDirectories = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask)
+        guard let applicationSupport = applicationSupportDirectories.first else {
+            print("application support failure")
+            return
+        }
+        
+        // construct model file path
+        let modelURL = applicationSupport.appendingPathComponent("MobileNet" + ".mlmodelc")
+        
+        // ensure the model file exists
+        guard fileManager.fileExists(atPath: modelURL.path) else {
+            print("file does not exist")
+            return
+        }
+        
+        // load model file from disk
+        guard let model = try? MLModel(contentsOf: modelURL) else {
+            print("failed to set model")
+            return
+        }
+        
         visualRecognition.classifyLocally(image: image, owners: [classifierId], failure: failure) { classifiedImages in
-            let filtered = classifiedImages.images[0].classifiers[0].classes.prefix(2) //  limit results to 2
-            
-            // Update UI on main thread
-            DispatchQueue.main.async {
-                if filtered.isEmpty {
-                    self.classificationLabel.text = "Unrecognized."
-                } else {
-                    let descriptions = filtered.map { classification in
-                        // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
-                        return String(format: "  (%.4f) %@", classification.score, classification.classification)
-                    }
-                    self.classificationLabel.text = "Classification:\n" + descriptions.joined(separator: "\n")
-                }
-            }
+            print(classifiedImages)
+//            let filtered = classifiedImages.images[0].classifiers[0].classes.prefix(2) //  limit results to 2
+//
+//            // Update UI on main thread
+//            DispatchQueue.main.async {
+//                if filtered.isEmpty {
+//                    self.classificationLabel.text = "Unrecognized."
+//                } else {
+//                    let descriptions = filtered.map { classification in
+//                        // Formats the classification for display; e.g. "(0.37) cliff, drop, drop-off".
+//                        return String(format: "  (%.4f) %@", classification.score, classification.classification)
+//                    }
+//                    self.classificationLabel.text = "Classification:\n" + descriptions.joined(separator: "\n")
+//                }
+//            }
         }
     }
     
